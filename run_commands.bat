78aaf6a52f837f9e060b8847b00c694c0254afd6
CryptoCommander007 <rivantped@gmail.com>
2024-05-16 08:55:06 -0400

    BANNER-CARRUSEL-INICIO-PAGINA-BIP0001

---
+++ run_commands.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "BANNER-CARRUSEL-INICIO-PAGINA-BIP0001"
+git branch -M main
+git remote add origin https://github.com/CryptoCommander007/BANNER-CARRUSEL-INICIO-PAGINA-BIP0001.git
+git push -u origin main
